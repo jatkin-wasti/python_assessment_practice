95226c42159b83d294db44fce2411b9360161af3
jatkin-wasti <jatkin-wasti@spartaglobal.com>
2020-11-12 10:45:34 +0000

    added dictionary task

--- practice_questions.py
+++ practice_questions.py
@@ -9,6 +9,10 @@
 # num_tuple = (1, 2, 3, 4, 5)
 # print(num_tuple)
 # tuples are immutable so we can't add to it without converting it to a list and back again
+# my_list = list(num_tuple)
+# my_list.append(2.5)
+# num_tuple = tuple(my_list)
+# print(num_tuple)
 
 # Creating a dictionary and changing a value
 # num_dict = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5}
@@ -17,9 +21,13 @@
 # print(num_dict)
 
 # Create a set and output values from it until you output 3
-rand_set = {1, 2, 3, 4, 5}
-for num in rand_set:
-    print(num)
-    if num == 3:
-        break
+# rand_set = {1, 2, 3, 4, 5}
+# for num in rand_set:
+#     print(num)
+#     if num == 3:
+#         break
 
+# Declare dictionary of a shopping list with 3 items and print out a price for one of the items
+shopping_list = {"Milk": 0.85, "Bread": 0.80, "Crisps": 1.00}
+print(type(shopping_list))
+print(shopping_list["Bread"])
