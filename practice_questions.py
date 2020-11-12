458862d906ffcf4e47f03261e0a0ac0c60a461f8
jatkin-wasti <jatkin-wasti@spartaglobal.com>
2020-11-12 11:00:39 +0000

    added class task

--- practice_questions.py
+++ practice_questions.py
@@ -28,10 +28,39 @@
 #         break
 
 # Declare dictionary of a shopping list with 3 items and print out a price for one of the items
-shopping_list = {"Milk": 0.85, "Bread": 0.80, "Crisps": 1.00}
-print(type(shopping_list))
-print(shopping_list["Bread"])
+# shopping_list = {"Milk": 0.85, "Bread": 0.80, "Crisps": 1.00}
+# print(type(shopping_list))
+# print(shopping_list["Bread"])
 
 # Replace the price for Bread with 1.45
-shopping_list["Bread"] = 1.45
-print(shopping_list["Bread"])
+# shopping_list["Bread"] = 1.45
+# print(shopping_list["Bread"])
+
+# Declare a function that takes one argument
+def add(val1, val2):
+    return val1 + val2
+
+
+print(add(6, 7))
+
+# Create Person class with name and age
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+test = Person("Jeff", 56)  # Instantiating an object of the class
+print(f"Hello {test.name}, you are {test.age} years old")
+
+# Create a Student class that inherits from the Person class
+class Student(Person):
+    def __init__(self, name, age, student_id, course):
+        super().__init__(name, age)
+        self.student_id = student_id
+        self.course = course
+
+
+student_test = Student("Jeff", 14, 212051, "DevOps")
+print(f"Hello {student_test.name}, you are {student_test.age} years old.\n"
+      f"Your student id is {student_test.student_id}, and you are enrolled in the {student_test.course} course")
